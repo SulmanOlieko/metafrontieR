293077a5e55de06b63d43c64333b2f6789cd8ebb
Sulman Olieko Owili <141807255+SulmanOlieko@users.noreply.github.com>
2026-02-27 17:59:44 +0300

    Update startup_msg.R

--- R/startup_msg.R
+++ R/startup_msg.R
@@ -1,28 +1,17 @@
 metafrontieRStartupMessage <- function() {
   art_lines <- c(
-"+-------------------------------------------------------------------------------------------------------------------------+",
-"|                                            /##                                                                 ##### /## |",
-"|                                          #/ ###                                              #              ######  / ## |",
-"|                           #             ##   ###                                     #      ###            /#   /  /  ## |",
-"|                          ##             ##                                          ##       #            /    /  /   ## |",
-"|                          ##             ##                                          ##                        /  /    /  |",
-"|### /### /###     /##   ######## /###    ###### ###  /###     /###   ###  /###     ######## ###       /##     ## ##   /   |",
-"| ##/ ###/ /##  / / ### ######## / ###  / #####   ###/ #### / / ###  / ###/ #### / ########   ###     / ###    ## ##  /    |",
-"|  ##  ###/ ###/ /   ###   ##   /   ###/  ##       ##   ###/ /   ###/   ##   ###/     ##       ##    /   ###   ## ###/     |",
-"|  ##   ##   ## ##    ###  ##  ##    ##   ##       ##       ##    ##    ##    ##      ##       ##   ##    ###  ## ##  ###  |",
-"|  ##   ##   ## ########   ##  ##    ##   ##       ##       ##    ##    ##    ##      ##       ##   ########   ## ##    ## |",
-"|  ##   ##   ## #######    ##  ##    ##   ##       ##       ##    ##    ##    ##      ##       ##   #######    #  ##    ## |",
-"|  ##   ##   ## ##         ##  ##    ##   ##       ##       ##    ##    ##    ##      ##       ##   ##            /     ## |",
-"|  ##   ##   ## ####    /  ##  ##    /#   ##       ##       ##    ##    ##    ##      ##       ##   ####    / /##/      ###|",
-"|  ###  ###  ### ######/   ##   ####/ ##  ##       ###       ######     ###   ###     ##       ### / ######/ /  ####    ## |",
-"|   ###  ###  ### #####     ##   ###   ##  ##       ###       ####       ###   ###     ##       ##/   ##### /    ##     #  |",
-"|                                                                                                           #              |",
-"|                                                                                                            ##            |",
-"+-------------------------------------------------------------------------------------------------------------------------+"
+"                       888              .d888                          888    d8b          8888888b. ",
+"                       888             d88P\"                           888    Y8P          888   Y88b",
+"                       888             888                             888                 888    888",
+"88888b.d88b.   .d88b.  888888  8888b.  888888 888d888 .d88b.  88888b.  888888 888  .d88b.  888   d88P",
+"888 \"888 \"88b d8P  Y8b 888        \"88b 888    888P\"  d88\"\"88b 888 \"88b 888    888 d8P  Y8b 8888888P\" ",
+"888  888  888 88888888 888    .d888888 888    888    888  888 888  888 888    888 88888888 888 T88b  ",
+"888  888  888 Y8b.     Y88b.  888  888 888    888    Y88..88P 888  888 Y88b.  888 Y8b.     888  T88b ",
+"888  888  888  \"Y8888   \"Y888 \"Y888888 888    888     \"Y88P\"  888  888  \"Y888 888  \"Y8888  888   T88b"
   )
   base_msg <- paste0(
     "\n\n* Please cite the 'metafrontieR' package as:\n",
-    "Owili SO. (2026). metafrontieR: Metafrontier Analysis in R. R package version 1.0.0.\n\n",
+    "Owili, SO. (2026). metafrontieR: Metafrontier Analysis in R. R package version 1.0.0.\n\n",
     "See also: citation(\"metafrontieR\")\n\n",
     "* For any questions, suggestions, or comments on the 'metafrontieR' package, you can contact the authors directly or visit:\n",
     "  https://github.com/SulmanOlieko/metafrontieR/issues\n"
@@ -36,7 +25,7 @@ metafrontieRStartupMessage <- function() {
     trimmed_art <- substr(art_lines, 1, console_width - 5)
     
     # 2. Add the ASCII ellipsis strictly to the last row
-    ellipsis <- " ## ## ##"
+    ellipsis <- "   88 88 88"
     e_len <- nchar(ellipsis)
     cut_point <- max(0, console_width - e_len)
     
@@ -73,7 +62,7 @@ metafrontieRStartupMessage <- function() {
   } else {
     msg <- paste0(
       "\n* Please cite the 'metafrontieR' package as:\n",
-      "Owili SO. (2026). metafrontieR: Metafrontier Analysis in R. R package version 1.0.0.\n\n",
+      "Owili, SO. (2026). metafrontieR: Metafrontier Analysis in R. R package version 1.0.0.\n\n",
       "See also: citation(\"metafrontieR\")\n\n",
       "* For any questions, suggestions, or comments on the 'metafrontieR' package, you can contact the authors directly or visit:\n",
       "  https://github.com/SulmanOlieko/metafrontieR/issues\n"
